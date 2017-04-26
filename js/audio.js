d75929d36081ce3c78a1f073c3029c60e03d40e1
Linchengyi <m15659856340@163.com>
2017-04-26 23:08:52 +0800

    Upload

---
+++ js/audio.js
@@ -0,0 +1,327 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-04-26 22:58:56
+ * @version $Id$
+ */
+
+/**
+ *  作者：@ZZITE
+ *  博客：https://zzite.github.io/
+ *  邮箱：zziteqaq@163.com
+ */
+
+window.onload = function () {
+
+	var audio = document.getElementById('audio'),           // 播放器                  
+	 	sourceList = audio.getElementsByTagName('source'),  // 音乐资源列表         
+	 	play = document.getElementById('play'),             // 播放按钮       
+	 	prev = document.getElementById('prev'),             // 上一曲        
+	 	next = document.getElementById('next'),             // 下一曲          
+	 	mute = document.getElementById('mute'),             // 静音              
+	 	voiceBar = document.getElementById('voiceBar'),     // 音量条                  
+	 	voicedBar = document.getElementById('voicedBar'),   // 当前音量         
+	 	musicBar = document.getElementById('musicBar'),     // 播放进度         
+	 	playedBar = document.getElementById('playedBar'), 	// 已播放进度       
+	 	loadBar = document.getElementById('loadBar'), 		// 加载进度条        
+	 	songName = document.getElementById('songName'), 	// 歌曲名           
+	 	playTime = document.getElementById('playTime'), 	//  已播音乐时间     
+	 	totalTime = document.getElementById('totalTime'), 	// 歌曲音乐总时间        
+	 	musicImg = document.getElementById('music-img'), 	// 音乐封面        
+	 	cycle = document.getElementById('cycle'),			// 循环按钮              
+	 	musicList = document.getElementById('musicList'),	//我的歌单列表
+	 	currentSrcIndex = 0;								//用于控制上下曲
+
+	 	// 网易云音乐模块
+		var keyword = document.getElementById('keyWord'); 	// 搜索歌名           
+		var searchBtn = document.getElementById('searchBtn');// 搜索按钮           
+		var result = document.getElementById('result'); 	// 结果部分                 
+     
+
+
+	// 是否循环 
+	audio.loop = false;
+
+	// 是否自动播放
+	audio.autoplay = false;
+
+	// 初始化音量
+	audio.volume = 0.5;
+	var voicedBarWidth = (audio.volume / 1) * voiceBar.clientWidth;
+	voicedBar.style.width = voicedBarWidth + 'px';
+
+	// 是否自动缓冲加载
+	audio.autobuffer = false;
+
+
+	// 播放 暂停
+	play.onclick = function () {
+		if (audio.paused) {
+			//第一次播放更新歌曲标题与时常
+			var fen = parseInt(audio.duration / 60);
+			var miao = parseInt(audio.duration % 60);
+			if (miao < 10) {
+			miao = '0'+miao;
+			}
+			totalTime.innerHTML = fen + ':' + miao;
+			songName.innerHTML = sourceList[currentSrcIndex].title;
+			this.innerHTML = '嘘';
+			musicImg.style.animation = 'Rotate 30s linear infinite';
+			datasong();
+			audio.play();
+			//播放即更新歌曲图片信息
+			
+		} else {
+			audio.pause();
+			this.innerHTML = '咻';
+			musicImg.removeAttribute('style');
+		}
+	};
+
+
+		//点击歌单列表播放选中歌曲
+	musicList.addEventListener('click', function (ev) {
+		var t = ev.target || ev.srcElement;
+		if (t.tagName.toLowerCase() === 'a') {
+		//这里返回所点击的a标签位于ul的第几个li中 ~ ~
+ 		var position = ([].indexOf.call(t.parentNode.parentNode.childNodes,t.parentNode));
+		//因为存在空白节点稍微做个计算使返回的数字为0，1，2，3..这样的顺序
+		var index = Math.floor(position / 2);
+		currentSrcIndex = index;
+		datasong();
+		musicTime();
+		audio.play();	
+		}
+	});
+
+	function datasong() {
+		currentSrc = sourceList[currentSrcIndex].getAttribute('src');
+		currentImg = sourceList[currentSrcIndex].getAttribute('data-img');
+		musicImg.setAttribute('src', currentImg);
+		audio.setAttribute('src', currentSrc);
+	}
+
+	// 下一曲
+	next.onclick = function () {
+		changeMusic('next');
+	};
+
+	// 上一曲
+	prev.onclick = function () {
+		changeMusic('prev');
+	};
+
+	// 切换歌曲
+	function changeMusic (direct) {
+		if (direct === 'next') {
+			++ currentSrcIndex > sourceList.length - 1 && (currentSrcIndex = 0); // 下一曲
+		} else {
+			-- currentSrcIndex < 0 && (currentSrcIndex = sourceList.length -1); // 上一曲
+		}
+		datasong();
+		audio.play();
+		musicTime();
+	};
+
+	// 音量调节（增加黄色现在音量显示）
+	voiceBar.onclick = function (event) {
+		var voiceBarWidth = voiceBar.clientWidth;
+		var newVolume = (event.offsetX / voiceBarWidth);
+		audio.volume = newVolume;
+
+		// 音量大小更新
+		var voicedBarWidth = (audio.volume / 1) * voiceBarWidth;
+		voicedBar.style.width = voicedBarWidth + 'px';
+		
+	};
+
+	// 播放进度控制
+	musicBar.onclick = function (event) {
+		var musicBarWidth = musicBar.clientWidth;
+		var newCurrentTime = (event.offsetX / musicBarWidth) * audio.duration;
+		audio.currentTime = newCurrentTime;
+		var playedBarWidth = (audio.currentTime / audio.duration) * musicBarWidth;
+		playedBar.style.width = playedBarWidth + 'px';
+	};
+
+	// 播放进度实时更新(修改为歌曲播放时开启定时器，暂停和页面load时清除定时器)
+	setInterval(function updatePlayedBar (){
+		var musicBarWidth = musicBar.clientWidth;
+		var playedBarWidth = (audio.currentTime / audio.duration) * musicBarWidth;
+		playedBar.style.width = playedBarWidth + 'px';
+		if (audio.currentTime % 60 < 10) {
+			playTime.innerHTML = parseInt(audio.currentTime / 60) + ':0' + parseInt(audio.currentTime % 60);
+		} else {
+			playTime.innerHTML = parseInt(audio.currentTime / 60) + ':' + parseInt(audio.currentTime % 60);
+		}
+		//如果是时间结束，并且是非单曲循环，自动下一曲
+		if (audio.currentTime === audio.duration && !audio.loop) {
+			next.onclick();
+		}
+	}, 1000);
+	
+	// 静音
+	mute.onclick = function () {
+		if (!audio.muted) {
+			audio.muted = true;
+			voicedBar.style.width = 0 +'px';
+		}else {
+			audio.muted = false;
+			var voiceBarWidth = voiceBar.clientWidth;
+
+			// 音量大小更新
+			var voicedBarWidth = (audio.volume / 1) * voiceBarWidth;
+			voicedBar.style.width = voicedBarWidth + 'px';
+		}
+	};
+
+	// 判断文件缓冲进度
+	setInterval(function updateCache () {
+		var buffered, percent;
+		// 已缓冲部分
+		audio.readyState == 4 && (buffered = audio.buffered.end(0));
+
+		// 已缓冲百分百
+		audio.readyState == 4 && (percent = Math.round(buffered / audio.duration * 100) + '%');
+		loadBar.style.width = (Math.round(buffered / audio.duration * 100) * musicBar.clientWidth * 0.01) + 'px';
+	}, 1000);
+
+	// 计算音乐时间
+
+
+	//播放更换歌曲时，时间歌名显示部分更新
+	function musicTime() {
+		// 更换播放歌曲
+		songName.innerHTML = sourceList[currentSrcIndex].title;
+		// 播放时间显示
+		audio.addEventListener("canplay", function(){
+			var sc=audio.duration;
+
+			var fen = parseInt(sc / 60);
+			var miao = parseInt(sc % 60);
+			if (miao < 10) {
+				totalTime.innerHTML = fen + ':0' + miao;
+			}else {
+				totalTime.innerHTML = fen + ':' + miao;
+			}
+			play.innerHTML = '嘘';
+			musicImg.style.animation = 'Rotate 30s linear infinite';
+		});
+	}
+
+	// 是否单曲循环
+	cycle.onclick = function () {
+		if (audio.loop) {
+			audio.loop = false;
+			this.innerHTML = '循环';
+		} else {
+			audio.loop = true;
+			this.innerHTML = '单曲';
+		}
+	};
+
+//简单的交互
+	musicImg.addEventListener('mouseover',function() {
+	//var arr = shishi.src.split("/");// 	
+	if (musicImg.src.indexOf('miao.png') > 0) {
+	cuteCat.innerHTML = '乖巧'; 
+	}
+	});
+	musicImg.addEventListener('mouseout',function() {
+		cuteCat.innerHTML = '';
+	});
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// 利用委托来为立即播放绑定事件
+	result.addEventListener('click', function (ev) {
+		var t = ev.target || ev.srcElement;
+		//转化小写再比较更安全
+		if (t.tagName.toLowerCase() === 'a') {
+			var oMusicSrc = result.getAttribute('data-audio');
+			var oMusicImg = result.getAttribute('data-img');
+			var oMusicName = result.getAttribute('data-music');
+			var oSinger = result.getAttribute('data-singer');
+			musicImg.setAttribute('src',oMusicImg);
+			songName.innerHTML = oMusicName + '-' + oSinger;
+			audio.setAttribute('src', oMusicSrc);
+			audio.play();
+			musicTime();
+			t.innerHTML='正在播放';
+		}
+
+	});
+	
+
+	
+	// 搜索按钮
+	
+ 	keyword.addEventListener('keydown',function(e){
+   		if(!e){
+    	e = window.event;
+   	}
+   		if((e.keyCode || e.which) == 13){
+			var value = keyword.value;
+			if (!value) {
+				alert('关键词不能为空');
+				return;
+			}
+			var url = "http://s.music.163.com/search/get/";
+			var data = {
+				"type": 1,
+				"limit": 1,
+				"s": value,
+				"callback": "jsonpcallback"
+			};
+			var buffer = [];
+			for (var key in data) {
+				buffer.push(key + '=' + encodeURIComponent(data[key]));
+			}
+			var fullpath = url + '?' + buffer.join('&');
+			CreateScript(fullpath);
+		}
+   	});
+
+	searchBtn.addEventListener('click', function () {
+		var value = keyword.value;
+		if (!value) {
+			alert('关键词不能为空');
+			return;
+		}
+		var url = "http://s.music.163.com/search/get/";
+		var data = {
+			"type": 1,
+			"limit": 1,
+			"s": value,
+			"callback": "jsonpcallback"
+		};
+		var buffer = [];
+		for (var key in data) {
+			buffer.push(key + '=' + encodeURIComponent(data[key]));
+		}
+		var fullpath = url + '?' + buffer.join('&');
+		CreateScript(fullpath);
+	});
+    //JSONP
+	function CreateScript (src) {
+		var el = document.createElement('script');
+		el.src = src;
+		el.async = true;
+		el.defer = true;
+		document.body.appendChild(el);
+	}
+	
+};
